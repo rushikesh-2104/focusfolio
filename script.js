ecd9a473637eddcc9ea722991e9943820dee67d1
Rushikesh <hrishi210402@gmail.com>
2025-06-19 13:46:05 +0530

    first

---
+++ script.js
@@ -0,0 +1,43 @@
+let cursor = document.querySelector("#cursor");
+document.addEventListener("mousemove",function(dets){
+    gsap.to(cursor,{
+        x:dets.x,   //cursor element will move accordingly with x and y axis points
+        y:dets.y,
+        duration:1,  //for more smoothness 
+        ease:"elastic.out"  //for cursor movement effects
+    })
+})
+
+
+var tl = gsap.timeline()
+  tl.from("h1", {
+    y: -20,
+    opacity: 0,
+    duration: 0.5,
+  });
+
+  tl.from("h4", {
+    y: -20,
+    opacity: 0,
+    duration: 0.6,
+    stagger: 0.2
+  });
+
+
+gsap.to(".section2 .category",{
+    transform:"translateX(-265%)",
+    scrollTrigger:{                 //use to give effects on scrolling based
+        trigger:".section2",           //element to be triggered (write only parent wihle using pin)
+        scroller:"body",            //on which part
+        // markers:"true",             //use to marks the start and end points to view scroll
+        start:"top 0%",             //adjust the starting point 
+        end:"top -170%",            //adjust the ending point 
+        scrub:5,                    //used to give complete scroll based effect
+        pin:true                  //used to pin the element till the effect gets over
+    }
+})
+
+
+
+
+
